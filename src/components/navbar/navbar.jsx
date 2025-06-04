66e21b8c772922f7e5c6e314d9b28e7fae07ba95
Rio <riomaruyama@hotmail.com>
2025-06-03 22:17:30 -0700

    added logo and made navbar scrollable and routed to specific sections of the website

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,9 +4,11 @@ import styles from './navbar.module.css'
 export default function Navbar() {
     return (
         <div className= {styles.navbar}>
-            <div className={styles.navlogo}></div>
+            <div> 
+                <img src= 'public\images\logo.png' alt= 'logo' className={styles.navlogo}/> 
+            </div>
             <ul className ={styles.navlinks}>
-                <li><Link to='Home' className={styles.link}>Home</Link></li>
+                <li><Link to='Home' offset= {-62} className={styles.link}>Home</Link></li>
                 <li><Link to='Projects' className={styles.link}>Projects</Link></li>
                 <li><Link to='Skills' className={styles.link}>Skills</Link></li>
                 <li><Link to='Socials' className={styles.link}>Socials</Link></li>
--- src/sections/home/home.jsx
+++ src/sections/home/home.jsx
@@ -1,7 +1,16 @@
 export default function Home() {
     return (
     <div id='Home'> 
-        <h1>this is HOme</h1>
+        <h1>A little about me...</h1>
+
+        <p>I’m currently a data science major at 
+            Simon Fraser University eager to gain more experience 
+            as a data analyst, and future data scientist. 
+            My goal is to be able to help others with the power of data! 
+            During my free time, you can find me getting hotpot with friends 
+            (Big Way and Pot Empire are my second home) or struggling to 
+            speak Chinese.
+        </p>
     </div>
     )
 }

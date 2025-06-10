bb1507df29d9177794c49054189523aab4c694a5
Rio <riomaruyama@hotmail.com>
2025-06-10 01:01:52 -0700

    added first project

--- src/sections/projects/projects.jsx
+++ src/sections/projects/projects.jsx
@@ -1,15 +1,38 @@
+import styles from './projects.module.css'
+import { Link } from 'react-scroll'
+
 export default function Projects() {
+
+    const projects = [
+        {
+            name: "Find Your Matcha",
+            link: "https://github.com/justinnebaltazar/find-your-matcha",
+            image: "public/images/project_image/findyourmatcha.png"
+        }
+    ];
+
+
     return (
     <div id='Projects'>
-        <h1>this is Projects</h1>
-        <p>I’m currently a data science major at 
-                Simon Fraser University eager to gain more experience 
-                as a data analyst, and future data scientist. 
-                My goal is to be able to help others with the power of data! 
-                During my free time, you can find me getting hotpot with friends 
-                (Big Way and Pot Empire are my second home) or struggling to 
-                speak Chinese.
-        </p>
+        <h1 className={styles.title}>Projects</h1>
+        <div className={styles.projectlist}>
+            <ul>
+                {projects.map((project, index) => (
+                    <li key={index} className={styles.project}>
+                        <a href={project.link} target="_blank" rel="noopener noreferrer">
+                            <div className={styles.imageContainer}> 
+                                <img 
+                                src = {project.image} 
+                                alt={project.name} 
+                                className={styles.projectImage}/>
+                            </div>
+                        </a>
+                        <a href={project.link} target="_blank" rel="noopener noreferrer" className={styles.projectName}>{project.name}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+
     </div>
     )
 }

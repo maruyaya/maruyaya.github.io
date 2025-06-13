7481235f9921c1dedf3fff81351c960ce3fea171
Rio <riomaruyama@hotmail.com>
2025-06-13 12:07:06 -0700

    fixes #32

--- src/sections/projects/projects.jsx
+++ src/sections/projects/projects.jsx
@@ -35,21 +35,23 @@ export default function Projects() {
     return (
     <div id='Projects'>
         <h1 className={styles.title}>Projects</h1>
-        <div className={styles.projectcontainer}>
-                {projects.map((project, index) => (
-                        <div key={index} className={styles.project}>
-                            <a href={project.link} target="_blank" rel="noopener noreferrer">
-                                <div className={styles.imageContainer}> 
-                                    <img 
-                                    src = {project.image} 
-                                    alt={project.name} 
-                                    className={styles.projectImage}/>
-                                </div>
-                            </a>
-                            <a href={project.link} target="_blank" rel="noopener noreferrer" className={styles.projectName}>{project.name}</a>
-                            <p className={styles.description}>{project.description}</p>
-                        </div>
-                ))}
+        <div className={styles.projectcontainerwrapper}>
+            <div className={styles.projectcontainer}>
+                    {projects.map((project, index) => (
+                            <div key={index} className={styles.project}>
+                                <a href={project.link} target="_blank" rel="noopener noreferrer">
+                                    <div className={styles.imageContainer}> 
+                                        <img 
+                                        src = {project.image} 
+                                        alt={project.name} 
+                                        className={styles.projectImage}/>
+                                    </div>
+                                </a>
+                                <a href={project.link} target="_blank" rel="noopener noreferrer" className={styles.projectName}>{project.name}</a>
+                                <p className={styles.description}>{project.description}</p>
+                            </div>
+                    ))}
+            </div>
         </div>
 
     </div>

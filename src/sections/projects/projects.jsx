6c89b57e6c8c3c8e8997122b0585d4ce3292df83
Rio <riomaruyama@hotmail.com>
2025-06-12 18:28:01 -0700

    added borders for each project

--- src/sections/projects/projects.jsx
+++ src/sections/projects/projects.jsx
@@ -6,13 +6,15 @@ export default function Projects() {
         {
             name: "Find Your Matcha",
             link: "https://github.com/justinnebaltazar/find-your-matcha",
-            image: "public/images/project_image/findyourmatcha.png"
+            image: "public/images/project_image/findyourmatcha.png",
+            description: "Created a Matcha reviewing platform for UBC's CMD-F hackathon."
         },
 
         {
             name: "Webscraping Tutorial Article",
             link: "public/project_links/WebscrapingTutorial.pdf",
-            image: "public/images/project_image/webscrapetutorial.png"
+            image: "public/images/project_image/webscrapetutorial.png",
+            description: "For my STAT 260 final project, we had to make a webscraping tutorial for people with minimal technical knowledge."
         }
     ];
 
@@ -21,21 +23,20 @@ export default function Projects() {
     <div id='Projects'>
         <h1 className={styles.title}>Projects</h1>
         <div className={styles.projectcontainer}>
-            <div>
                 {projects.map((project, index) => (
-                    <li key={index} className={styles.project}>
-                        <a href={project.link} target="_blank" rel="noopener noreferrer">
-                            <div className={styles.imageContainer}> 
-                                <img 
-                                src = {project.image} 
-                                alt={project.name} 
-                                className={styles.projectImage}/>
-                            </div>
-                        </a>
-                        <a href={project.link} target="_blank" rel="noopener noreferrer" className={styles.projectName}>{project.name}</a>
-                    </li>
+                        <div key={index} className={styles.project}>
+                            <a href={project.link} target="_blank" rel="noopener noreferrer">
+                                <div className={styles.imageContainer}> 
+                                    <img 
+                                    src = {project.image} 
+                                    alt={project.name} 
+                                    className={styles.projectImage}/>
+                                </div>
+                            </a>
+                            <a href={project.link} target="_blank" rel="noopener noreferrer" className={styles.projectName}>{project.name}</a>
+                            <p className={styles.description}>{project.description}</p>
+                        </div>
                 ))}
-            </div>
         </div>
 
     </div>

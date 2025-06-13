c265740ce4cbd744611adb8feddce7f24c12c09d
Rio <riomaruyama@hotmail.com>
2025-06-13 11:40:27 -0700

    completed projects design (might add more later) fixes #5

--- src/sections/projects/projects.jsx
+++ src/sections/projects/projects.jsx
@@ -6,15 +6,28 @@ export default function Projects() {
         {
             name: "Find Your Matcha",
             link: "https://github.com/justinnebaltazar/find-your-matcha",
-            image: "public/images/project_image/findyourmatcha.png",
-            description: "Created a Matcha reviewing platform for UBC's CMD-F hackathon."
+            image: "public/project_images/findyourmatcha.png",
+            description: `Created a Matcha reviewing platform for UBC's CMD-F hackathon.`
         },
 
         {
             name: "Webscraping Tutorial Article",
             link: "public/project_links/WebscrapingTutorial.pdf",
-            image: "public/images/project_image/webscrapetutorial.png",
-            description: "For my STAT 260 final project, we had to make a webscraping tutorial for people with minimal technical knowledge."
+            image: "public/project_images/webscrapetutorial.png",
+            description: `For my STAT 260 final project, we had to make a 
+                            webscraping tutorial for people with minimal technical knowledge.
+                            `
+        },
+
+                {
+            name: "DSSS x SFU AIESEC's Business Hackathon",
+            link: "public/project_links/GlobalyticsSlide.pdf",
+            image: "public/project_images/Globalytics.png",
+            description: `The Data Science Student Society and SFU AIESEC 
+            hosted a business hackathon where we came up with solutions on how 
+            SFU AIESEC can increase the number of applicants for their exchange program.
+            This was a great learning opportunity for my team and I where we got to put our 
+            coursework in action.`
         }
     ];
 

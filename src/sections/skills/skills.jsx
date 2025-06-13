a4ec2ffc52a45f179255e447838aeaa085f0f323
Rio <riomaruyama@hotmail.com>
2025-06-13 11:50:47 -0700

    centered title

--- src/sections/skills/skills.jsx
+++ src/sections/skills/skills.jsx
@@ -1,15 +1,10 @@
+import styles from './skills.module.css'
+
 export default function Skills() {
     return (
     <div id='Skills'>
-        <h1>this is SKills</h1>
-        <p>I’m currently a data science major at 
-            Simon Fraser University eager to gain more experience 
-            as a data analyst, and future data scientist. 
-            My goal is to be able to help others with the power of data! 
-            During my free time, you can find me getting hotpot with friends 
-            (Big Way and Pot Empire are my second home) or struggling to 
-            speak Chinese.
-        </p>
+        <h1 className={styles.title}>Skills</h1>
+        
     </div>
     )
 }

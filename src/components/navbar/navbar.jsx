9b9eace4b457d333f2d1f729d8fee376c275307e
Rio <riomaruyama@hotmail.com>
2025-09-21 20:22:03 -0700

    edited project image size

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,22 +1,24 @@
 import { Link } from 'react-scroll'
 import styles from './navbar.module.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function Navbar() {
 
     //*change nav colour when scrolling *//
     const [color, setColour] = useState(false)
 
-    const changeColour = () => {
-        if (window.scrollY >= document.documentElement.clientHeight*0.7) {
-            setColour(true)
+    useEffect(() => {
+        const changeColour = () => {
+            if (window.scrollY >= document.documentElement.clientHeight * 0.7) {
+                setColour(true)
+            } else {
+                setColour(false)
+            }
         }
-        else {
-            setColour(false)
-        }
-    }
 
-    window.addEventListener('scroll', changeColour)
+        window.addEventListener('scroll', changeColour)
+        return () => window.removeEventListener('scroll', changeColour)
+    }, [])
 
    
 
@@ -26,7 +28,7 @@ export default function Navbar() {
                 <img src= '/images/logo.png' alt= 'logo' className={styles.navlogo}/> 
             </div>
             <ul className ={styles.navlinks}>
-                <li><Link to='Home' offset= {-62} className={styles.link}>Home</Link></li>
+                <li><Link to='Home' offset= {-70} className={styles.link}>Home</Link></li>
                 <li><Link to='Projects' className={styles.link}>Projects</Link></li>
                 <li><Link to='Skills' className={styles.link}>Skills</Link></li>
                 <li><Link to='Socials' className={styles.link}>Socials</Link></li>
